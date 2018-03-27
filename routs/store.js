1b82e9d776c7fb4ad637f8dc97bdc0cc3b3dd138
NazariyMural <nazik_94@ukr.net>
2018-03-27 16:21:50 +0300

    New branch

--- routs/store.js
+++ routs/store.js
@@ -4,12 +4,38 @@ const Product = require("../models/Product");
 const mongoose = require("mongoose");
 // const checkAuth = require("../middleware/check_auth");
 
+// router.get("/store", (req, res, next) => {
+//   Product.find()
+//     .then(data => {
+//       res.status(200).send(data);
+//     })
+//     .catch(err => res.send(400).send(err));
+// });
+
 router.get("/store", (req, res, next) => {
-  Product.find()
-    .then(data => {
-      res.status(200).send(data);
-    })
-    .catch(err => res.send(400).send(err));
+  const perPage = 5;
+  let productData = req.params.id.split(",");
+  let resultsPagination = productData[0];
+  const page = resultsPagination || 0;
+
+  Product.find({
+    name: {
+      $regex: new RegExp(productData[1], "i")
+    }
+  })
+    .skip(perPage * page - page)
+    .limit(perPage)
+    .exec((err, products) => {
+      Product.count().exec((err, count) => {
+        if (err) return next(err);
+        res.send({
+          product: products,
+          current: page,
+          pages: Math.ceil(count / perPage),
+          count: count
+        });
+      });
+    });
 });
 
 router.post("/store", (req, res, next) => {
