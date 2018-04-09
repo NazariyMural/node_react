ae78676591afb3ce6a78782aabf2ae985c706a2a
NazariyMural <nazik_94@ukr.net>
2018-04-09 16:49:41 +0300

    4:

--- app.js
+++ app.js
@@ -123,4 +123,9 @@ app.use((err, req, res, next) => {
   res.render("error");
 });
 
+const path = require("path");
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
 module.exports = app;
